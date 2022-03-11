6c5923da23abd52ce05096a5b185b6b91886e2eb
VINO <wangbingya@yibaishun.cn>
2022-03-11 22:10:56 +0800

    neo4j

---
+++ src/test/java/io/github/vino42/springbootdemos/SpringBootDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vino42.springbootdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemosApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
